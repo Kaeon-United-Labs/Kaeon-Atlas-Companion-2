1d9aba096108b9bef886a40f5d26fa4dec056966
Jesse Dinkin <kaeon.ace@gmail.com>
2025-01-16 22:29:33 -0500

    Kaeon

--- Kaeon Atlas Companion 2/2 - Wonders/1 - External/2 - META/metaUtils.js
+++ Kaeon Atlas Companion 2/2 - Wonders/1 - External/2 - META/metaUtils.js
@@ -2,11 +2,44 @@
 
 	Persistent Loop - Task Backlog - Multiple Agents - Local or Remote
 	Find various agents... hugging face?
+	Pass to other agent one of many options - Options & sub options from atlas
+	Agent is a chat log / With limit?
 
 */
 
-// STUB
+var philosophersStone = require("kaeon-united")("philosophersStone");
+
+function chat(log) {
+
+}
+
+function run() {
+
+	/*
+
+		Perpetual w/ callbacks:
+	
+			Get agents from atlas
+			Get tasks from atlas
+
+			Agent: { id#, log[] }
+			Task: { id#, Desc, log[], finished: t/f }
+
+			For each agent, rank each task & execute best tasks via atlas.
+
+			Execute: Get options via atlas, Pass Action to atlas
+
+			Option: "Desc"
+			Action: { "Desc", taskID# }
+
+			Packet: { META: { type: "type", value: (data) } }
+	
+	*/
+
+	// return intervals
+}
 
 module.exports = {
-
+	chat,
+	run
 };
